9e4d3126e9959ee7129593a0efca595fea06ba29
SheiDominguez <sgdominguezj@gmail.com>
2022-04-21 08:26:49 -0500

    Nuevas opciones en el menu
    Nuevas opciones en el menu del supervisor y nueva forma de añadir horas trabajadas

--- EmployeeControl/Program.cs
+++ EmployeeControl/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EmployeeControl
 {
@@ -40,7 +42,8 @@ namespace EmployeeControl
                     Console.WriteLine("3 - Eliminar un empleado");
                     Console.WriteLine("4 - Registrar horas laboradas");
                     Console.WriteLine("5 - Registrar nuevo empleado");
-                    Console.WriteLine("6 - Salir");
+                    Console.WriteLine("6 - Reporte: Horas por proyecto.");
+                    Console.WriteLine("7 - Agregar proyecto");
 
                     var isNumber = Int32.TryParse(Console.ReadLine(), out int opcion);
                     if (!isNumber)
@@ -113,7 +116,7 @@ namespace EmployeeControl
                             #region Registro de horas del supervisor
                             Console.Clear();
                             Console.WriteLine("Registro de horas");
-                            User.RegisterHours(supervisor.Id);
+                            AgregarHorasAlProyecto(user);
                             #endregion
                             break;
                         case 5:
@@ -124,6 +127,14 @@ namespace EmployeeControl
                             #endregion
                             break;
                         case 6:
+                            #region Horas por Proyecto
+                            Console.Clear();
+                            Console.WriteLine("Registro de horas por proyecto");
+                            ReporteHorasPorProyecto();
+                            #endregion
+                            break;
+                        case 7:
+                            AgregarProyecto();
                             break;
                         default:
                             Console.WriteLine("Opción no válida");
@@ -132,20 +143,33 @@ namespace EmployeeControl
                 }
                 else
                 {
+
                     #region Registro de horas del empleado
                     var employee = new Employee(user.Name, user.MiddleName, user.Rol);
-                    Console.WriteLine("¿Desea registrar sus horas laboradas de la semana? (s/n)");
-                    var isChar = Char.TryParse(Console.ReadLine(), out char opcion);
-                    if (!isChar)
+
+                    #region Menú de empleado
+                    Console.WriteLine("¿Que operación deseas ejecutar?");
+                    Console.WriteLine("1 - Registrar horas");
+                    Console.WriteLine("2 - Salir");
+
+                    var isNumber = Int32.TryParse(Console.ReadLine(), out int opcion);
+                    if (!isNumber)
                     {
-                        Console.WriteLine("Opción no válida");
+                        Console.WriteLine("Por favor proporciona un número válido.");
                         return;
                     }
+                    #endregion Menú de empleado
 
-                    if (opcion.Equals('n') || opcion.Equals('N'))
-                        return;
-
-                    User.RegisterHours(user.Id);
+                    switch (opcion)
+                    {
+                        case 1:
+                            AgregarHorasAlProyecto(user);
+                            break;
+                        case 2:
+                        default:
+                            return;
+                    }
+                    
                     #endregion
                 }
 
@@ -162,6 +186,124 @@ namespace EmployeeControl
                     terminaPrograma = true;
                 #endregion Terminar ejecución del programa
             }
+        }
+
+        private static void ReporteHorasPorProyecto()
+        {
+            var list = Supervisor.GetHoursPerProject();
+            if (list.Any())
+            {
+                list.ForEach(h =>
+                {
+                    Console.WriteLine($"Proyecto: {h.Project}   ID: {h.IdProject}");
+                    Console.WriteLine($"    Employee: {h.IdEmployee} - {h.Employee}     Hours: {h.Hours}");
+                });
+            }
+            else
+            {
+                Console.WriteLine("No hay horas registradas");
+            }
+        }
+
+        private static void AgregarHorasAlProyecto(User user)
+        {
+            var listHoras = new List<HoursPerProjectDto>();
+            var week = new WeeklyRegister();
+            int horasTotal = 0;
+            var proyectos = User.GetProjects();
+            string[] days = new string[5] { "Lunes","Martes","Miércoles","Jueves","Viernes"};
+            Console.Clear();
+            if (proyectos.Any())
+            {
+                int countLunes = 0, countMartes = 0, countMiercoles = 0, countJueves = 0, countViernes = 0;
+                for (int j = 0; j < days.Length; j++)
+                {
+                    Console.WriteLine($"Registro de horas: {days[j]}");
+                    bool continuar = true;
+                    while (continuar)
+                    {
+                        Console.WriteLine("Selecciona el proyecto al que deseas agregar horas");
+                        for (int i = 0; i < proyectos.Count; i++)
+                        {
+                            Console.WriteLine($"{i + 1}. {proyectos.ElementAt(i).Nombre}");
+                        }
+                        var proyectoPosicion = IntentarObtenerInt(Console.ReadLine()) - 1;
+                        var proyecto = proyectos.ElementAt(proyectoPosicion);
+
+                        Console.WriteLine($"¿Cuantas horas quiere registrar al proyecto {proyecto.Nombre}?");
+                        var horas = IntentarObtenerInt(Console.ReadLine());
+
+                        switch (days[j])
+                        {
+                            case "Lunes":
+                                countLunes += horas;
+                                break;
+                            case "Martes":
+                                countLunes += horas;
+                                break;
+                            case "Miércoles":
+                                countLunes += horas;
+                                break;
+                            case "Jueves":
+                                countLunes += horas;
+                                break;
+                            case "Viernes":
+                                countLunes += horas;
+                                break;
+                        }
+
+                        listHoras.Add(new HoursPerProjectDto { IdEmployee = user.Id, Employee = user.Name, IdProject = proyecto.IdProyecto, Project = proyecto.Nombre, Hours = horas });
+                        horasTotal += horas;
+                        Console.WriteLine($"Ha registrado un total de {horasTotal} horas.");
+                        Console.WriteLine("\n\n¿Desea registrar mas horas? s/n");
+                        continuar = Console.ReadLine().ToLower().Equals("s") ? true : false;
+                    }
+                }
+                Console.WriteLine("Agrega una breve descripción de tu trabajo");
+                var descripcion = Console.ReadLine();
+
+                week.HoursMonday = countLunes;
+                week.HoursTuesday = countMartes;
+                week.HoursWednesday = countMiercoles;
+                week.HoursThursday = countJueves;
+                week.Hoursfriday = countViernes;
+                week.UserId = user.Id;
+                week.Description = descripcion;
+                week.HoursValidated = false;
+
+                User.RegisterHours(week);
+                User.AddHoursToProject(listHoras);
+                Console.WriteLine("Horas registradas correctamente");
+            }
+            else
+            {
+                Console.WriteLine("No hay projectos a los cuales asignarles horas.");
+            }
+            
+        }
+
+        private static void AgregarProyecto()
+        {
+            Console.WriteLine("Agregar Proyecto\n");
+
+            Console.WriteLine("¿Que nombre tendrá el proyecto?");
+            var nombre = Console.ReadLine();
+            var isValid = string.IsNullOrEmpty(nombre);
+
+            if (isValid)
+            {
+                Console.WriteLine("El nombre no puede ser vacio");
+                return;
+            }
+
+            var proyecto = new Projects(nombre);
+            Supervisor.AddProject(proyecto);
+            Console.WriteLine("Proyecto agregado correctamente.");
+        }
+
+        private static int IntentarObtenerInt(string input)
+        {
+            return Int32.TryParse(input, out int posicion) ? posicion : throw new ApplicationException("No se pudo castear el id correctamente");
         }
     }
 }
---
+++ EmployeeControl/Projects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeControl
+{
+    public class Projects
+    {
+        public int IdProyecto { get; set; }
+        public string Nombre { get; set; }
+        private int _idProyectoSeed = 1;
+        public static List<Projects> ProjectsList = new List<Projects>();
+
+        public Projects(string nombre)
+        {
+            if (string.IsNullOrEmpty(nombre))
+                throw new ApplicationException("El nombre no puede ser vacio.");
+
+            IdProyecto = _idProyectoSeed;
+            Nombre = nombre;
+            _idProyectoSeed++;
+        }
+
+
+    }
+}
